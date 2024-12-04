96ab9cd4de8dd0d2d3f7cca28152c60241aef844
Avery442 <avery442@proton.me>
2024-12-04 17:57:22 +0000

    A kinda functional DVD logo simulator D:

--- pet.py
+++ pet.py
@@ -1,57 +1,66 @@
 import tkinter as tk
-import time
+import random
 
-class pet():
+class Pet():
     def __init__(self):
         self.window = tk.Tk()
-        #code below generates string for each frame in gif
-        self.moveleft = [tk.PhotoImage(file='duck-left.gif', format='gif -index %i' % (i)) for i in range(10)]
-        self.moveright = [tk.PhotoImage(file='duck-right.gif', format='gif -index %i' % (i)) for i in range(10)]
-        self.frame_index = 0 #setting starting frame
-        self.img = self.moveleft[self.frame_index]      #starting direction gif
-        self.timestamp = time.time()
+        
+        # Load the image for the pet
+        self.img = tk.PhotoImage(file='stella.png')  # Replace with the path to your image
+        
+        self.timestamp = 0
         self.window.config(background='black')
         self.window.wm_attributes('-transparentcolor', 'black')
-        self.window.overrideredirect(True)      #makes window frameless
-        self.window.attributes('-topmost', True)        #puts window on top
-        self.label = tk.Label(self.window, bd=0, bg='black')    #creates a label as a container for a gif
-        #starting points
-        self.x = 1040
-        self.y = 670
-        self.window.geometry('128x128+{}+{}'.format(str(self.x), str(self.y)))
+        self.window.overrideredirect(True)  # Makes window frameless
+        self.window.attributes('-topmost', True)  # Puts window on top
+        
+        self.label = tk.Label(self.window, bd=0, bg='black')  # Creates a label as a container for the image
         self.label.configure(image=self.img)
         self.label.pack()
+        
+        # Initial position of the pet (randomized)
+        self.x = random.randint(100, 1400)
+        self.y = random.randint(100, 700)
+        
+        # Initial speed of the pet
+        self.speed = 2
+        
+        # Random directions for movement
+        self.dx = random.choice([-1, 1])  # Horizontal movement direction (left or right)
+        self.dy = random.choice([-1, 1])  # Vertical movement direction (up or down)
+        
+        # Set the initial geometry for the pet's window
+        self.window.geometry('1920x1080+{}+{}'.format(str(self.x), str(self.y)))
+        
+        # Start the movement loop
         self.window.after(0, self.update)
-        self.dir = -1   #starting direction
+        
         self.window.mainloop()
 
-    def changetime(self, direction):
-        if time.time() > self.timestamp + 0.05:
-            self.timestamp = time.time()
-            self.frame_index = (self.frame_index + 1) % 5   #speed of frames change
-            self.img = direction[self.frame_index]
-
-    def changedir(self):
-        self.dir = -(self.dir)
-
-    def go(self):
-        self.x = self.x + self.dir
-        if self.dir <0:
-            direction = self.moveleft
-        else:
-            direction = self.moveright
-        self.changetime(direction)
-
-
     def update(self):
-        self.go()
-        if self.x == 560 or self.x == 1060:
-            self.changedir()
+        # Move the pet in the current direction
+        self.x += self.dx * self.speed
+        self.y += self.dy * self.speed
 
-        self.window.geometry('128x128+{}+{}'.format(str(self.x), str(self.y)))
+        # Bounce the pet off the left and right edges
+        if self.x <= 0 or self.x >= 1500 - 128:  # Subtracting image width (128px) from window width
+            self.dx = -self.dx  # Reverse horizontal direction
+        
+        # Bounce the pet off the top and bottom edges
+        if self.y <= 0 or self.y >= 800 - 128:  # Subtracting image height (128px) from window height
+            self.dy = -self.dy  # Reverse vertical direction
+        
+        # Update the position of the pet's window
+        self.window.geometry('128x128+{}+{}'.format(int(self.x), int(self.y)))
+        
+        # Update the image (no change, it's always the same image)
         self.label.configure(image=self.img)
-        self.label.pack()
-        self.window.after(10, self.update)  #10 is frames number for my gif
+        
+        # Update at regular intervals
+        self.window.after(20, self.update)  # 20 milliseconds between updates
+        
+        # Keep the window on top of others
         self.window.lift()
 
-pet()
+# Initialize the pet
+Pet()
